5f10a0ce76edab22547bab8c66b5dd50542bdc1b
Daniel Chavez <d.chavez562@yahoo.com>
2016-06-23 14:08:12 -0500

    connection string for temporary database added

---
+++ server/modules/connection.js
@@ -0,0 +1,9 @@
+var connectionString = '';
+
+if(process.env.DATABASE_URL != undefined) {
+  connectionString = process.env.DATABASE_URL + "?ssl=true";
+} else {
+  connectionString = 'postgres://gprbmrbaeemuux:k2Skt178phw_S6kIrJmpkErYg1@ec2-54-225-211-218.compute-1.amazonaws.com:5432/d3u173chd9eq5m';
+}
+
+module.exports = connectionString;
