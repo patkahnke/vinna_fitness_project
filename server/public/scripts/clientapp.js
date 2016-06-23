8462722cb7917d30d83792d48cb49a2ff35951bd
“HannahCORRECT_EMAIL=“hannahrspringer@gmail.com <>
2016-06-23 16:15:59 -0500

    Deleted unnecessary html files and moved all assessment html files to one html file.

--- server/public/scripts/clientapp.js
+++ server/public/scripts/clientapp.js
@@ -8,33 +8,8 @@ myApp.config(['$routeProvider', function($routeProvider) {
       controller: "HomeController"
     })
 
-    .when('/assessment1', {
-      templateUrl: '/views/assessment1.html',
-      controller: "AssessmentController"
-    })
-
-    .when('/assessment2', {
-      templateUrl: '/views/assessment2.html',
-      controller: "AssessmentController"
-    })
-
-    .when('/assessment3', {
-      templateUrl: '/views/assessment3.html',
-      controller: "AssessmentController"
-    })
-
-    .when('/assessment4', {
-      templateUrl: '/views/assessment4.html',
-      controller: "AssessmentController"
-    })
-
-    .when('/assessment5', {
-      templateUrl: '/views/assessment5.html',
-      controller: "AssessmentController"
-    })
-
-    .when('/assessment6', {
-      templateUrl: '/views/assessment6.html',
+    .when('/assessment', {
+      templateUrl: '/views/assessment.html',
       controller: "AssessmentController"
     })
 
