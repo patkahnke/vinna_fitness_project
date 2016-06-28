e06a18d613bd36df1248f3b1421ec31b80110b4b
Samantha Klein <saradurga@gmail.com>
2016-06-28 16:51:50 -0500

    logout feature

--- server/public/scripts/factories/userFactory.js
+++ server/public/scripts/factories/userFactory.js
@@ -10,26 +10,27 @@ myApp.factory('UserFactory', function ($http) {
   var publicAPI = {
 
     currentUser: userStatus,
-
+    //checks to see if a user is authenticated, returns true or false
     checkLoggedIn: function () {
       return userStatus.loggedIn;
     },
-
+    //send a req to the server to find an authenticated user
     isLoggedIn: function () {
       return $http.get('/login');
     },
-
+    //sets userStatus.loggedIn to true (logged in) or false (not logged in)
     setLoggedIn: function (value) {
           userStatus.loggedIn = value;
         },
-
+    //logout
     logout: function () {
-          return $http.get('/auth/logout');
+          return $http.get('/login/logout');
         },
-
+    //sets userStatus.admin to true (admin) or false (trainer)
     setAdmin: function (admin) {
            userStatus.isAdmin = admin;
         },
+    //checks to see if a user is an admin
     checkAdmin: function () {
       return userStatus.isAdmin;
     }
