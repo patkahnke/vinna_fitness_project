a3e94603a5e17e457d1159f93b152ddb943cf7d0
Hannah <hannahrspringer@gmail.com>
2016-07-01 23:56:53 -0500

    Updated header page routing--logo text now routes to home page depending on which user type is logged in, and updated hidden navbar for admin.

--- server/public/scripts/controllers/headerController.js
+++ server/public/scripts/controllers/headerController.js
@@ -6,22 +6,31 @@ myApp.controller('HeaderController', ['$scope', '$http', '$window', '$location',
   $scope.trainerHide = true;
   $scope.hiddenAdmin = true;
 
-  userFactory.checkAdmin();
-  if (userFactory.checkAdmin === true) {
-    $scope.hiddenAdmin = false;
-  } else {
-    $scope.hiddenAdmin = true;
-  }
-
   userFactory.isLoggedIn()
   .then(function (response) {
       if (response.data.name !== undefined) {
         $scope.hidden = false;
+        if (response.data.admin === true) {
+          $scope.hiddenAdmin = false;
+        }
       } else {
         $scope.hidden = true;
       }
 
   });
+
+  $scope.routeHome = function() {
+    if (userFactory.checkLoggedIn() === true) {
+      if ($scope.hiddenAdmin === true) {
+      $location.path('/assessment');
+      }
+      else {
+      $location.path('/admin');
+      }
+    } else {
+      $location.path('/');
+    }
+};
 
   $scope.logout = function () {
     userFactory.logout()
