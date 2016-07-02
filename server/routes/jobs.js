c88e9cbd5d1ce97cf0899a1f60da997b4b2aed53
Daniel Chavez <d.chavez562@yahoo.com>
2016-07-01 19:21:44 -0500

    got selected companies jobs to populate the selected company page and adding a job is functioning

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -4,12 +4,16 @@ var pg = require('pg');
 var connectionString = require('../modules/connection');
 
 
-router.get('/', function(req, res) {
+router.get('/:id', function(req, res) {
+    var id = req.params.id;
     pg.connect(connectionString, function(err, client, done) {
         if (err) {
             res.sendStatus(500);
         }
-        client.query("SELECT * FROM job ",
+        client.query('SELECT * FROM job ' +
+                     'WHERE company_id = ($1) ' +
+                     'ORDER BY id ASC',
+                     [id],
          function(err, result) {
             if (err) {
                 console.log(err, "error retrieving existing job from database");
@@ -21,16 +25,21 @@ router.get('/', function(req, res) {
     });
 });
 
-router.post('/', function (req, res) {
-  var company = req.body;
+router.post('/:id', function (req, res) {
+  var id = req.params.id;
+  var job = req.body;
   pg.connect(connectionString, function (err, client, done) {
     if (err) {
       res.sendStatus(500);
     }
 
-    client.query( 'INSERT INTO company (name, location, email) ' +
-                  'VALUES ($1, $2, $3)',
-                   [company.name, company.location, company.email],
+    client.query( 'INSERT INTO job (title, company_id, job_email, ' +
+                  'min_deep_squat, min_hurdle_step, min_inline_lunge, ' +
+                  'min_shoulder_mob, min_straight_leg, min_rotary_stab, ' +
+                  'min_trunk_push_up, min_composite, core_subtest, ' +
+                  'shoulder_subtest, low_body_subtest, other_subtest) ' +
+                  'VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)',
+                   [job.title, id, job.email, job.minDeepSquat, job.minHurdleStep, job.minInlineLunge, job.minShoulderMob, job.minActiveStraightLegRaise, job.minRotaryStability, job.minTrunkStabilityPushup, job.compositeScore, job.coreSubtest, job.shoulderSubtest, job.lowerBodySubtest, job.otherSubtest],
                  function (err, result) {
                    done();
                    if (err) {
