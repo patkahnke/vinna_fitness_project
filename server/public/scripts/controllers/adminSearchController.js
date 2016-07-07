41fdc256680bf8bbb0146095308c78ddff8169db
Samantha Klein <saradurga@gmail.com>
2016-07-07 14:04:21 -0500

    search controller added

--- server/public/scripts/controllers/adminSearchController.js
+++ server/public/scripts/controllers/adminSearchController.js
@@ -1,8 +1,14 @@
-myApp.controller('AdminController', ['$scope', '$http', '$location', 'ApplicantFactory', 'UserFactory',  function($scope, $http, $location, ApplicantFactory, UserFactory)
+myApp.controller('AdminSearchController', ['$scope', '$http', 'AdminDataFactory', 'UserFactory', '$location', '$window', function($scope, $http, AdminDataFactory, UserFactory, $location, $window)
 {
+  //injections
+  $scope.dataFactory = AdminDataFactory;
   userFactory = UserFactory;
-  $scope.username = userFactory.currentUser.username;
+  //scope variables
+  $scope.applicants = [];
+  $scope.first_name = '';
+  $scope.last_name = '';
 
+  //authenticated?
   if (userFactory.checkLoggedIn() === true) {
     if (userFactory.checkAdmin() === false) {
       $location.path('/trainer');
@@ -12,6 +18,31 @@ myApp.controller('AdminController', ['$scope', '$http', '$location', 'ApplicantF
   };
 
 
+  //Get applicant results from database
+  $scope.getApplicant = function() {
+    var query = {
+      first_name: $scope.first_name,
+      last_name: $scope.last_name
+    }
+    console.log('here');
+    $http.post('/search', query)
+      .then(function (response) {
+        console.log('GET /applicant', response.data);
+        $scope.applicants = response.data;
+        $scope.results = true;
+        $scope.first_name = '';
+        $scope.last_name = '';
+      });
+  }
+
+  $scope.viewApplicantModal = {
+    modalShown : false
+  };
+
+  $scope.toggleViewApplicantModal = function(applicant) {
+    $scope.viewApplicantModal.modalShown = !$scope.viewApplicantModal.modalShown;
+    $scope.applicant = applicant;
+  }
 
 
-}]);
+  }]);
